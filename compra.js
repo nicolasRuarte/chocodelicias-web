0a1b2d16037057b23137db101ab86b3660146d6d
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-07-29 19:28:54 -0300

    feat: Agregando funcionalidad de cambio de tema

--- compra.js
+++ compra.js
@@ -1,31 +1,44 @@
-const buyButtons = document.querySelectorAll("buy");
-const addToCartButtons = document.querySelectorAll("add-to-cart");
+import { createToggleThemeButton } from "./script.js";
 
+const addToCartButtons = document.querySelectorAll("add-to-cart");
 const products = document.querySelectorAll(".product");
 const cartList = document.querySelector("#cart-list");
+const buyButton = document.querySelector("#buy");
+const total = document.querySelector("#total");
 
-function addCartElement() {
-    const pTypeIndex = 3;
-    const pPriceIndex = 5;
 
-    const total = document.querySelector("#total");
+function addCartElement(product) {
+    const productInfo = product.childNodes[3];
+    console.log(productInfo.childNodes);
+
+    const pTypeIndex = 1;
+    const pPriceIndex = 3;
+
     let totalAmount = parseInt(total.textContent);
-    totalAmount += parseInt(product.childNodes[5].textContent);
+    totalAmount += parseInt(productInfo.childNodes[pPriceIndex].textContent);
     total.textContent = totalAmount;
 
     const cartElement = document.createElement("li");
-    const cartElementText = document.createTextNode(`${product.childNodes[pTypeIndex].textContent} ${product.childNodes[pPriceIndex].textContent}`)
+    const cartElementText = document.createTextNode(`${productInfo.childNodes[pTypeIndex].textContent} ${product.childNodes[pPriceIndex].textContent}`)
     cartElement.appendChild(cartElementText);
 
     cartList.appendChild(cartElement);
 }
 
-for (product of products) {
+for (const product of products) {
     product.addEventListener("click", (e) => {
         const target = e.target;
 
         if (target.className === "add-to-cart") {
-            addCartElement();
+            addCartElement(product);
         }
     });
 }
+
+buyButton.onclick = function () {
+
+    cartList.textContent = "";
+    total.textContent = "0";
+}
+
+createToggleThemeButton();
